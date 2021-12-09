d19e46f84d0d3df63d8387c4aee25674ca71b3a7
kawano-020 <orca.inc0510@gmail.com>
2021-12-10 04:49:32 +0900

    routing button

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <img
       src = "https://github-readme-stats.vercel.app/api/pin/?username=kawano-020&repo=api-workspace&show_owner=true&show_icons=true"
     />
+    <v-btn @click="$router.push('/doc')">{{"API Doc"}}</v-btn>
   </div>
 </template>
 
