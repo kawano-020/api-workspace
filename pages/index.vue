5c3ab7d01588d7a24510785ea43295550f590115
kawano-020 <orca.inc0510@gmail.com>
2021-12-10 02:09:19 +0900

    toppage edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>{{ "Welcome to WorkSpace" }}</h1>
+    <img
+      src = "https://github-readme-stats.vercel.app/api/pin/?username=kawano-020&repo=api-workspace&show_owner=true&show_icons=true"
+    />
+  </div>
 </template>
 
 <script lang="ts">
