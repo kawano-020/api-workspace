64102bbeaef6f8fe0b0506594e534e3fa26bf1ea
kawano-020 <orca.inc0510@gmail.com>
2021-12-14 19:40:33 +0900

    build setting修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  target: 'static',
+  target: 'server',
 
   router: {
     // https://nuxtjs.org/deployments/github-pages/
