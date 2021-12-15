83f54f382bba51e95b23a79cdcc465634684e8ce
kawano-020 <orca.inc0510@gmail.com>
2021-12-16 00:15:56 +0900

    doc修正

--- pages/doc.vue
+++ pages/doc.vue
@@ -3,8 +3,13 @@
     v-if="state.nodeEnv === 'production'"
     :src="`${$router.options.base}` + 'redoc.html'"
   />
-  <BaseContainer v-else>
-    {{ 'Only Production.' }}
+  <BaseContainer v-else class="develop">
+  <v-row class="caution" justify="center">
+      <v-icon left :size="50">mdi-alert-rhombus</v-icon>
+      <h1>
+        {{ 'Production Only.' }}
+      </h1>
+  </v-row>
   </BaseContainer>
 </template>
 
@@ -36,5 +41,12 @@ iframe {
   border: none;
   width: 100vw;
   height: calc(100vh - 64px);
+}
+.develop {
+  .caution {
+    h1 {
+      display: inline-block;
+    }
+  }
 }
 </style>
