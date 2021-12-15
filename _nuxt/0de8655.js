a432563c7c5acd8da8237fc560be59889a1c4e79
kawano-020 <kawano-020@users.noreply.github.com>
2021-12-15 12:40:59 +0000

    deploy: 97654b9f6835c313372819f913cd6bb438244c3b

--- _nuxt/0de8655.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{421:function(n,t,e){"use strict";e.r(t);var c=e(72),l=Object(c.a)({setup:function(){}}),o=e(71),component=Object(o.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
