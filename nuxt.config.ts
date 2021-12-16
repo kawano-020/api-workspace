3e2dc029157662e84e47ed1ab46af4a8a69ceece
kawano-020 <orca.inc0510@gmail.com>
2021-12-17 02:22:23 +0900

    titleTemplate削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - WorkSpace',
     title: 'WorkSpace',
     htmlAttrs: {
       lang: 'ja',
