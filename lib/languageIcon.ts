ee3839b783c5e404cba33cebf0f9421c5a6e9563
kawano-020 <orca.inc0510@gmail.com>
2021-12-18 03:33:40 +0900

    langTheme微調整

--- lib/languageIcon.ts
+++ lib/languageIcon.ts
@@ -5,7 +5,7 @@ export const languageThemes = {
   Dockerfile: { icon: 'mdi-docker', color: 'deep-purple darken-1' },
   Css: { icon: 'mdi-language-css3', color: 'blue' },
   JavaScript: { icon: 'mdi-language-javascript', color: 'yellow darken-2' },
-  Rust: { icon: 'mdi-language-rust', color: 'deep-orange darken-2' },
+  Rust: { icon: 'mdi-language-rust', color: 'orange lighten-2' },
   Python: { icon: 'mdi-language-python', color: 'blue darken-1' },
   Swift: { icon: 'mdi-language-swift', color: 'deep-orange' },
   Kotlin: { icon: 'mdi-language-kotlin', color: 'purple' },
